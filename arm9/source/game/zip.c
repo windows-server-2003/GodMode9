22995a810496e9d103b282f6eb93017a457cba1d
windows-server-2003 <rin2004@softbank.ne.jp>
2018-01-11 20:34:55 +0900

    fix buffer overflow(data abort) and other bugs

--- arm9/source/game/zip.c
+++ arm9/source/game/zip.c
@@ -5,11 +5,11 @@
 #include "zip.h"
 
 u8 zipExtractContent(const char* path, const char* extrpath, ZipLocalFileHeader hdr, u32 lfh_ptr, u32* flags) {
-    u32 FileDataStart_o;     // start offset of adtually data
+    u32 data_start_off;     // start offset of adtually data
     bool isdir;
     // these variables here are some path
     char name [256] = { 0 }; // content name (e.g. File : "testdir/testfile.txt" Dir : "testdir/testsubdir") (last slash will be removed in the process)
-    char* real_name;         // the real name of the content (e.g. File : "testfile.txt" Dir : "testsubdir")
+    char* real_name = NULL;         // the real name of the content (e.g. File : "testfile.txt" Dir : "testsubdir")
     char extr_path_full[256] = { 0 }; // the full path of the file (e.g. File : "0:/extract/testdir/testfile.txt" Dir : "0:/extract/testdir/testsubdir" (but not used))
     char extr_dir_full [256] = { 0 }; // the full path of dir of content or full path of the folder(e.g. File : "0:/extract/testdir" Dir : "0:/extract/testdir/testsubdir")
     
@@ -17,12 +17,12 @@ u8 zipExtractContent(const char* path, const char* extrpath, ZipLocalFileHeader 
    
     // get file name
     if (hdr.name_len > 255) return ZIP_ERROR_NAME_TOO_LONG; // check file name length
-	if (FileGetData(path, name, hdr.name_len, lfh_ptr + _ZIP_LFH_SIZE) != hdr.name_len) return ZIP_ERROR_FAILED_READ;
-	
+    if (FileGetData(path, name, hdr.name_len, lfh_ptr + _ZIP_LFH_SIZE) != hdr.name_len) return ZIP_ERROR_FAILED_READ;
+    
     // get if it is a file (by checking content name end char)
     char name_last = name[strlen(name)-1]; // last char : '/' means it must be a dir
     if (name_last == '/') isdir = true;
-	else isdir = false;
+    else isdir = false;
     
     // check zip version
     if (hdr.version == 0x0a) { // a non-compressed file
@@ -37,17 +37,17 @@ u8 zipExtractContent(const char* path, const char* extrpath, ZipLocalFileHeader 
     } else return ZIP_ERROR_CE6; // the content is encrypted, compressed or used ZIP64, can't extract.
     
     // get the start offset of actually data
-    FileDataStart_o = lfh_ptr + _ZIP_LFH_SIZE + hdr.name_len + hdr.extra_field_len;
+    data_start_off = lfh_ptr + _ZIP_LFH_SIZE + hdr.name_len + hdr.extra_field_len;
     
     // search for last '/' in content full name
-    snprintf(extr_path_full, 256, "%s/%s", extrpath, name);
+    snprintf(extr_path_full, 255, "%s/%s", extrpath, name);
     char* slash = strrchr(extr_path_full, '/');
     if (slash) {
-		real_name = slash+1;
-        strncpy(extr_dir_full, extr_path_full, slash-extr_dir_full-1);
-	} else return ZIP_ERROR_UNKNOWN; // no slash in the full path(impossible)
+        real_name = slash+1;
+        strncpy(extr_dir_full, extr_path_full, slash-extr_path_full);
+    } else return ZIP_ERROR_UNKNOWN; // no slash in the full path(impossible)
     
-	fvx_rmkdir(extr_path_full); // create dir
+    fvx_rmkdir(extr_dir_full); // create dir
     if (!isdir) { // if file, create file and inject the data
         if (PathExist(extr_path_full)) { // path already exists
             if (*flags & SKIP_ALL) return ZIP_NO_ERRORS;
@@ -59,7 +59,7 @@ u8 zipExtractContent(const char* path, const char* extrpath, ZipLocalFileHeader 
                 if (user_select == 1) {
                     do {
                         if (ShowStringPrompt(real_name, 255 - (strlen(extr_dir_full)), "Choose new destination name")) {
-							snprintf(extr_path_full, 256, "%s/%s", extrpath, real_name); // update path (only used one below here)
+                            snprintf(extr_path_full, 256, "%s/%s", extrpath, real_name); // update path (only used one below here)
                         }
                     } while (PathExist(extr_path_full));
                 } else if (user_select == 2) {
@@ -81,31 +81,32 @@ u8 zipExtractContent(const char* path, const char* extrpath, ZipLocalFileHeader 
         }
         
         if (!FileCreateDummy(extr_dir_full, real_name, hdr.size_original)) return ZIP_ERROR_FAILED_CREATE; // since once deleted the dir, the file shouldn't be exist
-        if (FileGetSize(path) < FileDataStart_o + hdr.size_compressed) return ZIP_ERROR_ZIP_TOO_SMALL; // Check the archive size to prevent show error in FileInjectFile()
-        if (!FileInjectFile(extr_path_full, path, 0, FileDataStart_o, hdr.size_original, NULL)) return ZIP_ERROR_FAILED_INJECT;
+        if (FileGetSize(path) < data_start_off + hdr.size_compressed) return ZIP_ERROR_ZIP_TOO_SMALL; // Check the archive size to prevent show error in FileInjectFile()
+		if (hdr.size_compressed != 0) {
+			if (!FileInjectFile(extr_path_full, path, 0, data_start_off, hdr.size_original, NULL)) return ZIP_ERROR_FAILED_INJECT;
+		}
     }
     return ZIP_NO_ERRORS; // succeed
 }
 
 bool ZipExtract(const char* path, const char* extrpath, u32* flags) {
     bool silent = (flags && (*flags & SILENT));
-	ZipLocalFileHeader hdr;
-	u32 lfh_ptr = 0;
-	u32 err_code = 0;
-	
+    ZipLocalFileHeader hdr;
+    u32 lfh_ptr = 0;
+    u32 err_code = 0;
+    
     // make sure there is a directory to extract(and also check write permissions)
     if (!CheckDirWritePermissions(extrpath)) return false; // once check here and never check below here
     fvx_rmkdir(extrpath);
-	
-	while (true) {
+    
+    while (true) {
         // local File Header check
-		if (FileGetData(path, &hdr, _ZIP_LFH_SIZE, lfh_ptr) != _ZIP_LFH_SIZE) return false; // reached the end of the file without End-of-central-dir-header
-		lfh_ptr += (_ZIP_LFH_SIZE + hdr.name_len + hdr.size_compressed + hdr.extra_field_len); // update file pointer
+        if (FileGetData(path, &hdr, _ZIP_LFH_SIZE, lfh_ptr) != _ZIP_LFH_SIZE) return false; // reached the end of the file without End-of-central-dir-header
         if (hdr.signature != _ZIP_LFH_SIG) { // not a LFH
             if (hdr.signature == _ZIP_DD_SIG) return false; // data descriptor is not supported
             return true; // reached at the end of LFHs
         }
-		
+        
         err_code = zipExtractContent(path, extrpath, hdr, lfh_ptr, flags);
         if (err_code == ZIP_ERROR_FAILED_READ) { // unexpected file reading error
             if (!silent) ShowPrompt(false, "Error\noffset:%x\nFile i/o error", lfh_ptr);
@@ -113,9 +114,15 @@ bool ZipExtract(const char* path, const char* extrpath, u32* flags) {
         }else if (err_code == ZIP_USER_ABORT) { // user abort
             return false;
         }else if (err_code != ZIP_NO_ERRORS) { // continuable error
-            if (!silent) {
-				if (!ShowPrompt(true, "Error occurred\n\nContinue to next content?")) return false;
+			// debug
+			if (err_code == ZIP_ERROR_FAILED_INJECT) {
+				ShowPrompt(false, "Debug\n \noffset : %lx\nLFH:%lx\n", lfh_ptr, hdr.signature);
 			}
+            if (!silent) {
+                if (!ShowPrompt(true, "Error occurred\n\nContinue to next content?\noffset:%lx", lfh_ptr)) return false;
+            }
         }
+        
+        lfh_ptr += (_ZIP_LFH_SIZE + hdr.name_len + hdr.size_compressed + hdr.extra_field_len); // update file pointer
     }
 }
--- arm9/source/game/zip.h
+++ arm9/source/game/zip.h
@@ -5,33 +5,33 @@
 #define _ZIP_DD_SIG  0x4B500003
 
 // errors
-#define	ZIP_NO_ERRORS            0
-#define	ZIP_USER_ABORT           1
-#define	ZIP_ERROR_FAILED_READ    2
+#define ZIP_NO_ERRORS            0
+#define ZIP_USER_ABORT           1
+#define ZIP_ERROR_FAILED_READ    2
 #define ZIP_ERROR_FAILED_CREATE  3
-#define	ZIP_ERROR_FAILED_INJECT  4
-#define	ZIP_ERROR_ZIP_TOO_SMALL  5
-#define	ZIP_ERROR_NAME_TOO_LONG  6
-#define	ZIP_ERROR_C              7 // compressed
-#define	ZIP_ERROR_CE             8 // compressed or encrypted
-#define	ZIP_ERROR_CE6            9 // compressed or encrypted or used ZIP64
-#define	ZIP_ERROR_UNKNOWN       10
+#define ZIP_ERROR_FAILED_INJECT  4
+#define ZIP_ERROR_ZIP_TOO_SMALL  5
+#define ZIP_ERROR_NAME_TOO_LONG  6
+#define ZIP_ERROR_C              7 // compressed
+#define ZIP_ERROR_CE             8 // compressed or encrypted
+#define ZIP_ERROR_CE6            9 // compressed or encrypted or used ZIP64
+#define ZIP_ERROR_UNKNOWN       10
 
 #define _ZIP_LFH_SIZE sizeof(ZipLocalFileHeader)
 
 typedef struct {
-	u32 signature;       // reserved (0x504B0304)
-	u8  version;         // 0x0a:file 0x14:dir or deflated file
-	u8  os;              // 0x00:DOS/OS2(FAT) 0x03:Unix/Linux 0x07:Mac 0x0a:Windows(NTFS) 0x13:OSX(Darwin)
-	u16 options_flags;   // bit0 : password, bit3 : Data descriptor, bit11 : UTF-8 as file names and comments
-	u16 comp_method;     // 0x0000:non-compressed 0x0008:Deflated
-	u16 last_mod_time;   // (bit0-4)*2:seconds bit5-10:minutes bit11-15:hours
-	u16 last_mod_date;   // bit0-4:days bit5-8:months (bit9-15)+1980:years
-	u32 crc32;           // file's CRC32
-	u32 size_compressed; // file's size after compressing
-	u32 size_original;   // file's original size
-	u16 name_len;        // the length of file(dir)'s name
-	u16 extra_field_len; // 0 if no extra fields
+    u32 signature;       // reserved (0x504B0304)
+    u8  version;         // 0x0a:file 0x14:dir or deflated file
+    u8  os;              // 0x00:DOS/OS2(FAT) 0x03:Unix/Linux 0x07:Mac 0x0a:Windows(NTFS) 0x13:OSX(Darwin)
+    u16 options_flags;   // bit0 : password, bit3 : Data descriptor, bit11 : UTF-8 as file names and comments
+    u16 comp_method;     // 0x0000:non-compressed 0x0008:Deflated
+    u16 last_mod_time;   // (bit0-4)*2:seconds bit5-10:minutes bit11-15:hours
+    u16 last_mod_date;   // bit0-4:days bit5-8:months (bit9-15)+1980:years
+    u32 crc32;           // file's CRC32
+    u32 size_compressed; // file's size after compressing
+    u32 size_original;   // file's original size
+    u16 name_len;        // the length of file(dir)'s name
+    u16 extra_field_len; // 0 if no extra fields
 } __attribute__((packed)) ZipLocalFileHeader;
 
 bool ZipExtract(const char* path, const char* extrpath, u32* flags);
