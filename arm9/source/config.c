7e08d6447a990203a149b712c6487993d3d22f95
windows-server-2003 <rin2004@softbank.ne.jp>
2018-04-29 16:51:15 +0900

    add save confirmation message

--- arm9/source/config.c
+++ arm9/source/config.c
@@ -39,10 +39,13 @@ void initConfig() {
 
 bool SaveConfig() {
     DirCreate("0:/gm9", "config");
-    return FileSetData("0:/gm9/config/brightness", &screen_brightness, sizeof(int) , 0, true) &&
-           FileSetData("0:/gm9/config/show_space", &show_space       , sizeof(bool), 0, true) &&
-           FileSetData("0:/gm9/config/jpn_clock" , &use_jpn_clock    , sizeof(bool), 0, true) &&
-           FileSetData("0:/gm9/config/font_path" , font_path         , 256         , 0, true);  
+    bool ret = FileSetData("0:/gm9/config/brightness", &screen_brightness, sizeof(int) , 0, true) &&
+			   FileSetData("0:/gm9/config/show_space", &show_space       , sizeof(bool), 0, true) &&
+		   	   FileSetData("0:/gm9/config/jpn_clock" , &use_jpn_clock    , sizeof(bool), 0, true) &&
+               FileSetData("0:/gm9/config/font_path" , font_path         , 256         , 0, true);
+			   
+	if (!ret) ShowPrompt(false, "Failed to save settings");
+	return ret;
 }
 
 bool LoadConfig() {
@@ -62,6 +65,7 @@ bool LoadConfig() {
 void ConfigMenu() {
     const int line_height = min(10, GetFontHeight() + 2);
     u32 sel = 0;
+	bool edited = false;
     
     u8 n_opt = 0;
     u32 option_brightness = ++n_opt - 1;
@@ -73,13 +77,6 @@ void ConfigMenu() {
     options[option_show_free ] = "Show free space";
     options[option_jpn_clock ] = "Use Japanese style clock";
 	
-	// back up old config
-	int screen_brightness_old = screen_brightness;
-	bool show_space_old = show_space;
-	bool use_jpn_clock_old = use_jpn_clock;
-	char font_path_old[256];
-	snprintf(font_path_old, 255, "%s", font_path);
-    
     ClearScreenF(true, true, COLOR_STD_BG);
 	char instr[256];
 	snprintf(instr, 255, "GodMode9 Configuration Menu\n \nSTART - Save settings\nY - Reset font\nX - Reset selected setting to default\nR+X - Reset all settings to default");
@@ -114,12 +111,18 @@ void ConfigMenu() {
         if (pad_state & BUTTON_DOWN) sel = (sel+1) % n_opt;
         else if (pad_state & BUTTON_UP) sel = (sel+n_opt-1) % n_opt;
         else if (pad_state & BUTTON_B) { // exit
-			
+			if (edited) {
+				if (ShowPrompt(true, "You have edited some settings.\nDo you want to save them ?")) {
+					if (!SaveConfig()) ShowPrompt(false, "Failed to save settings");
+				} else LoadConfig();
+			}
 			break;
 		}
 		else if (pad_state & BUTTON_START) { // save setttings
-			if (SaveConfig()) ShowPrompt(false, "All settings saved");
-			else ShowPrompt(false, "Failed to save settings");
+			if (SaveConfig()) {
+				ShowPrompt(false, "All settings saved");
+				edited = false;
+			} else ShowPrompt(false, "Failed to save settings");
 		}
         else if (pad_state & BUTTON_Y) { // reset font
             *font_path = '\0';
@@ -131,19 +134,32 @@ void ConfigMenu() {
                     free(pbm);
                 }
             } else SetFontFromPbm(NULL, 0); // from VRAM
+			
+			// redraw because font might be changed
+			ShowString(instr);
+			DrawStringF(ALT_SCREEN, 0, 0, COLOR_GREEN, COLOR_STD_BG, "GodMode9 Configuration Menu");
+			
 			SaveConfig();
         }
 		else if (pad_state & BUTTON_X && pad_state & BUTTON_R1) { // reset all settings
-			if (ShowPrompt(true, "Do you really want to\nreset all setttings to default ?"))
+			if (ShowPrompt(true, "Do you really want to\nreset all setttings to default ?")) {
 				initConfig();
-			SaveConfig();
+				
+				// redraw because font might be changed
+				ShowString(instr);
+				DrawStringF(ALT_SCREEN, 0, 0, COLOR_GREEN, COLOR_STD_BG, "GodMode9 Configuration Menu");
+				
+				SaveConfig();
+			}
 		}
 		else if (pad_state & BUTTON_X) { // reset current selected item to the default
 			if (sel == option_brightness) screen_brightness = -1;
 			else if (sel == option_show_free) show_space = false;
 			else if (sel == option_jpn_clock) use_jpn_clock = false;
+			edited = true;
 		}
         else if (pad_state & (BUTTON_LEFT | BUTTON_RIGHT)) { // change value
+			edited = true;
             if (sel == option_brightness) {
                 screen_brightness += ((pad_state & BUTTON_RIGHT) ? 1 : -1);
                 if (screen_brightness < -1) screen_brightness = 15;
